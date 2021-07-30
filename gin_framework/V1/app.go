19d663404f13c1539c50f5bd872e96d17840b7e5
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-07-31 00:03:24 +0430

    add videoRouter to api

--- gin_framework/V1/app.go
+++ gin_framework/V1/app.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"V1/routers/videoRouter"
+	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,5 +10,11 @@ func main() {
 	server := gin.New()
 	server.Use(gin.Recovery(), gin.Logger())
 	//server.Use(middleware func)    ==> add custom middleware
+	api := server.Group("api/V1")
+	videoRouter.SetVideoRouter(api)
+	err := server.Run(":8080")
+	if err != nil {
+		fmt.Println("error occur in run server")
+	}
 
 }
