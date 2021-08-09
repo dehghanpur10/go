79b415ddae2358e8e546000109b0cf33fe9bb553
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-08-09 13:07:44 +0430

    add scan code

--- dynamoDB/scan/main.go
+++ dynamoDB/scan/main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"dynamoDB/createDynamoDB"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 )
 
 func main()  {
 	dynamoClient := createDynamoDB.Dynamo()
-	scanInput := &dynamodb.QueryInput{
-
+	filter := expression.Name("age").LessThanEqual(expression.Value(22)).And(expression.Name("id").Equal(expression.Value("5")))
+	expr,err := expression.NewBuilder().WithFilter(filter).Build()
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-	scan, err := dynamoClient.Query(scanInput)
+	scanInput := &dynamodb.ScanInput{
+		ExpressionAttributeValues: expr.Values(),
+		ExpressionAttributeNames: expr.Names(),
+		FilterExpression: expr.Filter(),
+		IndexName: aws.String("age-index"),
+		TableName: aws.String("User"),
+	}
+	scan, err := dynamoClient.Scan(scanInput)
 	if err != nil {
 		return
 	}
-	_=scan
+	fmt.Println(scan)
 }
